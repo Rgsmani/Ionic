d5175e61db7b670a94287a213d27f3aa8306b550
Manikandan <rgsmanikandan@gmail.com>
2020-04-29 09:31:10 +0530

    Home page changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { CoreServiceService } from "./services/core-service.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private coreService: CoreServiceService
   ) {
     this.initializeApp();
   }
@@ -23,5 +25,15 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+    // this.coreService.getDataByCountry().subscribe((data) => {
+    //   console.log('data', data);
+    // });
+
+    // this.coreService.getCountryWiseList().subscribe((data) => {
+    //   console.log('data', data);
+    // });
   }
 }
--- src/app/services/core-service.service.ts
+++ src/app/services/core-service.service.ts
@@ -7,9 +7,34 @@ import { Observable } from 'rxjs';
 })
 export class CoreServiceService {
 
+  APIBaseUrl = 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/'; 
+  total = 'total';
+  stats = 'stats';
+
   constructor(private httpClient: HttpClient) { }
 
+  getDataByCountry(country?: string): Observable<any> {
+    const headers = {
+      'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+      'x-rapidapi-key': '679dc7ca64msh113e2536b6d5704p1f7566jsn302c7431d338'
+    };
+    return this.httpClient.get(
+      this.APIBaseUrl + this.total + '?country=' + country,
+      { headers }
+    );
+  }
 
+
+  getCountryWiseList(country?: string): Observable<any> {
+    const headers = {
+      'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+      'x-rapidapi-key': '679dc7ca64msh113e2536b6d5704p1f7566jsn302c7431d338'
+    };
+    return this.httpClient.get(
+      this.APIBaseUrl + this.stats + '?country=' + country,
+      { headers }
+    );
+  }
 
 
 }
