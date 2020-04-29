6a582ccc6c271d3951eeb0e7ede8a2ffdb9ab1aa
manikandan.r <manikandan.r@photoninfotech.net>
2020-04-29 12:42:42 +0530

    home page changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoreServiceService } from '../services/core-service.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  data: any;
+
+  constructor(private coreService: CoreServiceService) {}
+
+  ngOnInit() {
+    this.coreService.getDataByCountry().subscribe((res) => {
+      this.data = res.data;
+      console.log('data', res);
+    });
+
+    // this.coreService.getCountryWiseList().subscribe((data) => {
+    //   console.log('data', data);
+    // });
+  }
 
 }
